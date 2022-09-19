744ab0a9516e310063a698ff9330b0c8401eb5c8
Bekhzod <Bekhzod1411@gmail.com>
2022-09-20 00:28:16 +0500

    mmmmmmmmm

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task1;
 public class UpdatedApp {
     public static void main(String[] args)
     {
-        System.out.println("fixMe");
-        System.out.println("I'm updated" + "\n" + "can be compiled. And can write to console");
+
+        System.out.println("I'm" + '\n' +  "updated" + '\n' + "can" + '\n'  + "be" + '\n' +  "compiled." + '\n' +  "And" + "can" +  write to console");
     }
 }
